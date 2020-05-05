192ed64e1cb8d2e4c2bc1f3b3c565a07eb9c07df
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-05-05 21:25:13 +0530

    variables

--- JS_Basic/script.js
+++ JS_Basic/script.js
@@ -1,1 +1,29 @@
 console.log("Hello from External Script");
+var fName = "Shahid";
+console.log(fName);
+var age = 22;
+console.log(age);
+var flag = false;
+
+if (flag == true) {
+    console.log("True");
+}
+else
+{
+    console.log("False");
+}
+
+hola="shahid";
+console.log(hola);
+
+
+var job;
+console.log(job);
+/* 
+/usr/bin/node JS_Basic/script.js 
+Hello from External Script
+Shahid
+22
+False
+shahid
+undefined */
