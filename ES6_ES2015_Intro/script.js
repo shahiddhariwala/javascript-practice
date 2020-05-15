b78e326fee553291374e85ed332955c1b538a436
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-05-15 23:38:15 +0530

    Es6 : Rest Paramter

--- ES6_ES2015_Intro/script.js
+++ ES6_ES2015_Intro/script.js
@@ -414,6 +414,56 @@ const  allSo = [h,...boxeso];
 
 Array.from(allSo).forEach(curr => curr.style.color = 'orange');
 
-//text color chnaged to orange
+//text color changed to orange
+
+
+/////////////////////////////////
+// Rest Parameter (in java we call it var-args)
+
+
+//ES5
+function isFullAge5() {
+    //console.log(arguments);
+    var argsArr = Array.prototype.slice.call(arguments);
+    
+    argsArr.forEach(function(cur) {
+        console.log((2016 - cur) >= 18);
+    })
+}
+
+
+//isFullAge5(1990, 1999, 1965);
+//isFullAge5(1990, 1999, 1965, 2016, 1987);
+
+
+//ES6
+function isFullAge6(...years) {
+    years.forEach(cur => console.log( (2016 - cur) >= 18));
+}
+
+isFullAge6(1990, 1999, 1965, 2016, 1987);
+
+
+//ES5
+function isFullAge5(limit) {
+    var argsArr = Array.prototype.slice.call(arguments, 1);
+
+    argsArr.forEach(function(cur) {
+        console.log((2016 - cur) >= limit);
+    })
+}
+
+
+//isFullAge5(16, 1990, 1999, 1965);
+isFullAge5(1990, 1999, 1965, 2016, 1987);
+
+
+//ES6
+function isFullAge6(limit, ...years) {
+    years.forEach(cur => console.log( (2016 - cur) >= limit));
+}
+
+isFullAge6(16, 1990, 1999, 1965, 2016, 1987);
+
 
 
